4897b4971afd2e6c3bbad032c07c7845851a75e9
DolicaAkelloEgwel <d.akelloegwel@lcc.arts.ac.uk>
2026-06-04 14:30:45 +0100

    dusting off mock knowledge...?

--- voicebox-wrapper/tests/test_voicebox.py
+++ voicebox-wrapper/tests/test_voicebox.py
@@ -1,5 +1,14 @@
+import requests
 import src.voicebox_wrapper.constants as constants
 from src.voicebox_wrapper.voicebox import VoiceBox
+
+
+class MockResponse:
+    status_code = None
+
+    @staticmethod
+    def json():
+        return dict()
 
 
 def test_set_url():
@@ -11,3 +20,55 @@ def test_set_url():
 def test_default_url():
     vb = VoiceBox()
     assert vb._url == constants.DEFAULT_URL
+
+
+def test_create_profile_sets_id(monkeypatch):
+
+    profile_id = "a-profile-id"
+
+    def mock_post(*args, **kwargs):
+        response = MockResponse()
+        response.status_code = 200
+        response.json = lambda: {"id": profile_id}
+        return response
+
+    monkeypatch.setattr(requests, "post", mock_post)
+
+    vb = VoiceBox()
+    profile = vb.create_profile("a-profile-name")
+
+    assert profile.id == profile_id
+
+
+def test_create_profile_with_custom_name():
+    pass
+
+
+def test_create_profile_with_default_name():
+    pass
+
+
+def test_create_profile_appends_list(monkeypatch):
+    def mock_post(*args, **kwargs):
+        response = MockResponse()
+        response.status_code = 200
+        response.json = lambda: {"id": "a-profile-id"}
+        return response
+
+    monkeypatch.setattr(requests, "post", mock_post)
+
+    vb = VoiceBox()
+    profile = vb.create_profile()
+    assert profile in vb._profiles
+
+
+def test_create_profile_failure():
+    pass
+
+
+def test_delete_profile_success():
+    pass
+
+
+def test_delete_profile_failure():
+    pass
