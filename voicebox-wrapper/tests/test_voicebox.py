6ccbb1b8ffb82c4a0eeb7e5e699a7c77cebb25ef
DolicaAkelloEgwel <d.akelloegwel@lcc.arts.ac.uk>
2026-06-04 11:43:57 +0100

    test for response failure

--- voicebox-wrapper/tests/test_voicebox.py
+++ voicebox-wrapper/tests/test_voicebox.py
@@ -25,3 +25,8 @@ def test_default_url():
 def test_given_code_200_then_success_returns_true(mock_response):
     mock_response.status_code = 200
     assert _success(mock_response)
+
+
+def test_given_code_not_200_then_success_returns_false(mock_response):
+    mock_response.status_code = 300
+    assert not _success(mock_response)
