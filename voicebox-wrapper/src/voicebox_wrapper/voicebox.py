72b29b59b33dea822980359776ead8445ccf7e04
DolicaAkelloEgwel <d.akelloegwel@lcc.arts.ac.uk>
2026-06-04 11:39:46 +0100

    moving success function

--- voicebox-wrapper/src/voicebox_wrapper/voicebox.py
+++ voicebox-wrapper/src/voicebox_wrapper/voicebox.py
@@ -8,18 +8,19 @@ from . import constants
 PROFILES = "/profiles/"
 
 
+def _success(response: requests.Response) -> bool:
+    return response.status_code == 200
+
+
 class VoiceBox:
     def __init__(self, url: str = constants.DEFAULT_URL):
         self._url = url
-
-    def _success(self, response: requests.Response) -> bool:
-        return response.status_code == 200
 
     def create_profile(self, name: str = str(uuid.uuid4())) -> requests.Response:
         data = {"name": name}
         response = requests.post(self._url + PROFILES, json=data)
 
-        if self._success(response):
+        if _success(response):
             self._id = response.json()["id"]
         else:
             # this means creating a profile failed
@@ -40,14 +41,14 @@ class VoiceBox:
             data=body,
             headers={"content-type": header},
         )
-        if not self._success(response):
+        if not _success(response):
             pass
         return response
 
     def begin_generating_audio(self, text: str) -> requests.Response:
         data = {"profile_id": self._id, "text": text}
         response = requests.post(self._url + "/generate", json=data)
-        if self._success(response):
+        if _success(response):
             self._generation_id = response.json()["id"]
         return response
 
@@ -56,12 +57,12 @@ class VoiceBox:
 
     def generation_complete(self, generation_id: str) -> bool:
         response = self._check_generation(generation_id)
-        if self._success(response):
+        if _success(response):
             return response.json()["status"] == "completed"
 
     def get_audio_path(self):
         response = self._check_generation()
-        if self._success(response):
+        if _success(response):
             return response.json()["audio_path"]
 
     def delete_profile(self, profile_id: str) -> requests.Response:
