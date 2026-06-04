a9c721a712160df6cce27f3d16737f0a4306498d
DolicaAkelloEgwel <d.akelloegwel@lcc.arts.ac.uk>
2026-06-04 12:14:31 +0100

    cleaning voicebox test

--- voicebox-wrapper/tests/test_voicebox.py
+++ voicebox-wrapper/tests/test_voicebox.py
@@ -1,14 +1,5 @@
-import pytest
 import src.voicebox_wrapper.constants as constants
-from src.voicebox_wrapper.voicebox import VoiceBox, _success
-
-
-@pytest.fixture
-def mock_response():
-    class MockResponse(object):
-        status_code = None
-
-    return MockResponse()
+from src.voicebox_wrapper.voicebox import VoiceBox
 
 
 def test_set_url():
@@ -20,13 +11,3 @@ def test_set_url():
 def test_default_url():
     vb = VoiceBox()
     assert vb._url == constants.DEFAULT_URL
-
-
-def test_given_code_200_then_success_returns_true(mock_response):
-    mock_response.status_code = 200
-    assert _success(mock_response)
-
-
-def test_given_code_not_200_then_success_returns_false(mock_response):
-    mock_response.status_code = 300
-    assert not _success(mock_response)
