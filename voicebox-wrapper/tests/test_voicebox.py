c9302d6fc918e2d27a04eb38fd00bc367728959c
DolicaAkelloEgwel <d.akelloegwel@lcc.arts.ac.uk>
2026-06-05 11:55:26 +0100

    profile deletion test

--- voicebox-wrapper/tests/test_voicebox.py
+++ voicebox-wrapper/tests/test_voicebox.py
@@ -1,5 +1,6 @@
 from unittest.mock import patch
 
+import pytest
 import src.voicebox_wrapper.constants as constants
 from src.voicebox_wrapper.voicebox import VoiceBox
 
@@ -8,6 +9,11 @@ class MockResponse:
     def __init__(self, code, json):
         self.status_code = code
         self.json = lambda: json
+
+
+class MockProfile:
+    def __init__(self, id):
+        self.id = id
 
 
 def test_set_url():
@@ -80,12 +86,31 @@ def test_create_profile_appends_list(mock_requests):
     assert profile in vb.profiles
 
 
-def test_create_profile_failure():
-    pass
+@patch("src.voicebox_wrapper.voicebox.requests")
+def test_create_profile_failure(mock_requests):
+    mock_requests.post.return_value = MockResponse(0, None)
+
+    vb = VoiceBox()
+
+    with pytest.raises(Exception):
+        vb.create_profile()
 
 
-def test_delete_profile_success():
-    pass
+@patch("src.voicebox_wrapper.voicebox.requests")
+def test_delete_profile_success(mock_requests):
+    mock_requests.delete.return_value = MockResponse(constants.REQUEST_SUCCESS, None)
+
+    profile_id_to_delete = "profile-id-to-delete"
+    profile = MockProfile(profile_id_to_delete)
+    vb = VoiceBox()
+    vb._profiles.append(profile)
+
+    vb.delete_profile(profile_id_to_delete)
+
+    assert profile not in vb.profiles
+    mock_requests.delete.assert_called_with(
+        vb._url + constants.PROFILES + profile_id_to_delete
+    )
 
 
 def test_delete_profile_failure():
